226041527cddcf74d4c06dfb84d4754427a40e39
Emiliano Prediger <epredigervianello@santandertecnologia.com.ar>
2023-10-09 22:47:47 -0300

    renombro theme

--- themes/colecciones-nacionales/views/Front/front_page_html.php
+++ themes/colecciones-nacionales/views/Front/front_page_html.php
@@ -43,7 +43,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://manzanadelasluces.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_manzana.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_manzana.png" alt="">
 					<h3>Complejo Histórico Cultural Manzana de las Luces</h3>
 					<div class="text-muted">
 						<p>Perú 294, CABA.<br/>
@@ -54,7 +54,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://museojesuitico.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_jesus_maria.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_jesus_maria.png" alt="">
 					<h3>Estancia de Jesús María - Museo Jesuítico Nacional</h3>
 					<div class="text-muted">
 						<p>Pedro de Oñate s/n°, Jesús María, Córdoba.<br/>
@@ -66,7 +66,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://casadelaindependencia.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_chi.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_chi.png" alt="">
 					<h3>Museo Casa Histórica de la Independencia</h3>
 					<div class="text-muted">
 						<p>Congreso 141, San Miguel de Tucumán, Tucumán.<br/>
@@ -77,7 +77,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://museoliniers.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_alta_gracia.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_alta_gracia.png" alt="">
 					<h3>Estancia de Jesús María - Museo Jesuítico Nacional</h3>
 					<div class="text-muted">
 						<p>Pedro de Oñate s/n°, Jesús María, Córdoba.<br />
@@ -93,7 +93,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://cabildonacional.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_cabildo.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_cabildo.png" alt="">
 					<h3>Museo del Cabildo de Buenos Aires y de la Revolución de Mayo</h3>
 					<div class="text-muted">
 						<p>Bolívar 65/ Hipólito Yrigoyen 511, CABA.<br />
@@ -104,7 +104,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://museodelhombre.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_museo_del_hombre.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_museo_del_hombre.png" alt="">
 					<h3>Museo del Hombre</h3>
 					<div class="text-muted">
 						<p>3 de Febrero 1370/78, CABA.<br />
@@ -115,7 +115,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="http://web.museoevita.org.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_inst_evita.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_inst_evita.png" alt="">
 					<h3>Museo Evita</h3>
 					<div class="text-muted">
 						<p>Lafinur 2988, CABA.<br />
@@ -126,7 +126,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://museodelnorte.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_museo_monrte.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_museo_monrte.png" alt="">
 					<h3>Museo Histórico del Norte</h3>
 					<div class="text-muted">
 						<p>Caseros 549, Salta.<br />
@@ -141,7 +141,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://museohistoriconacional.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_museo_historico.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_museo_historico.png" alt="">
 					<h3>Museo Histórico Nacional</h3>
 					<div class="text-muted">
 						<p>Defensa 1600, CABA.<br />
@@ -152,7 +152,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://museosarmiento.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_historico_sarmiento.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_historico_sarmiento.png" alt="">
 					<h3>Museo Histórico Sarmiento</h3>
 					<div class="text-muted">
 						<p>Juramento 2180, CABA.<br />
@@ -163,7 +163,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://museomalvinas.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_malvinas.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_malvinas.png" alt="">
 					<h3>Museo Malvinas e Islas del Atlántico Sur</h3>
 					<div class="text-muted">
 						<p>Av. del Libertador 8151, Espacio Memoria y Derechos Humanos (Ex ESMA), CABA.<br />
@@ -174,7 +174,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://museomitre.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_mitre.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_mitre.png" alt="">
 					<h3>Museo Mitre</h3>
 
 					<div class="text-muted">
@@ -189,7 +189,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://museoroca.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_roca.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_roca.png" alt="">
 					<h3>Museo Roca- Instituto de Investigaciones Históricas</h3>
 					<div class="text-muted">
 						<p>Vicente López 2220, CABA.<br />
@@ -200,7 +200,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://casanatalsarmiento.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_casa_natal_sarmiento.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_casa_natal_sarmiento.png" alt="">
 					<h3>Museo y Biblioteca Casa Natal de Sarmiento </h3>
 					<div class="text-muted">
 						<p>Sarmiento Sur 21, San Juan Ciudad, Prov. de San Juan.<br />
@@ -211,7 +211,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://museodelacuerdo.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_casa_del_acuerdo.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_casa_del_acuerdo.png" alt="">
 					<h3>Museo y Biblioteca Casa del Acuerdo de San Nicolás</h3>
 					<div class="text-muted">
 						<p>De la Nación 139 / 143. San Nicolás de los Arroyos, Buenos Aires.<br />
@@ -222,7 +222,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://museourquiza.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_san_jose.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_san_jose.png" alt="">
 					<h3>Palacio San José -Museo y Monumento Nacional Justo José de Urquiza</h3>
 
 					<div class="text-muted">
@@ -238,7 +238,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://museoyrurtia.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_yrurtia.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_yrurtia.png" alt="">
 					<h3>Museo Casa de Yrurtia</h3>
 					<div class="text-muted">
 						<p>O'Higgins 2390, CABA.<br />
@@ -249,7 +249,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://museodeltraje.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_traje.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_traje.png" alt="">
 					<h3>Museo de la Historia del Traje</h3>
 					<div class="text-muted">
 						<p>Chile 832, CABA.<br />
@@ -260,7 +260,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="http://museoartedecorativo.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_decorativo.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_decorativo.png" alt="">
 					<h3>Museo Nacional de Arte Decorativo</h3>
 					<div class="text-muted">
 						<p>Av. del Libertador 1902, CABA.<br />
@@ -271,7 +271,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://mnao.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_oriental.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_oriental.png" alt="">
 					<h3>Museo Nacional de Arte Oriental</h3>
 
 					<div class="text-muted">
@@ -284,20 +284,9 @@
 		<hr>
 		<div class="row">
 			<div class="col-sm-3 text-center">
-				<a href="https://www.bellasartes.gob.ar/" target="_blank" rel="noopener noreferrer">
-					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/logo_museo_b_artes_0_0.jpg" alt="">
-					<h3>Museo Nacional de Bellas Artes</h3>
-					<div class="text-muted">
-						<p>Av. Del Libertador 1473, CABA.<br />
-							(+54-11) 5288-9900.</p>
-					</div>
-				</a>
-			</div>
-			<div class="col-sm-3 text-center">
 				<a href="https://museodelgrabado.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_grabado.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_grabado.png" alt="">
 					<h3>Museo Nacional del Grabado</h3>
 					<div class="text-muted">
 						<p>Riobamba 985, CABA<br />
@@ -308,7 +297,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://museoterry.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_terry.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_terry.png" alt="">
 					<h3>Museo Regional de Pintura &quot;José Antonio Terry&quot;</h3>
 					<div class="text-muted">
 						<p>Rivadavia 352, Tilcara, Jujuy.<br />
@@ -319,7 +308,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://palaisdeglace.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_palais.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_palais.png" alt="">
 					<h3>Palais de Glace | Palacio Nacional de las Artes </h3>
 
 					<div class="text-muted">
@@ -334,7 +323,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://casadelbicentenario.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_cnb.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_cnb.png" alt="">
 					<h3>Casa Nacional del Bicentenario</h3>
 					<div class="text-muted">
 						<p>Riobamba 985, CABA.<br />
@@ -345,7 +334,7 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://museorojas.cultura.gob.ar/" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/museos_web_rojas.png" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/museos_web_rojas.png" alt="">
 					<h3>Museo Casa de Ricardo Rojas</h3>
 					<div class="text-muted">
 						<p>Charcas 2837, CABA.<br />
@@ -356,11 +345,22 @@
 			<div class="col-sm-3 text-center">
 				<a href="https://www.bn.gov.ar/biblioteca/museo" target="_blank" rel="noopener noreferrer">
 					<img style="max-width: 100%; max-height: 100%;"
-						 src="/themes/mytheme/assets/pawtucket/graphics/logo_museo_libro_lengua_0.jpg" alt="">
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/logo_museo_libro_lengua_0.jpg" alt="">
 					<h3>Museo del Libro y de la Lengua</h3>
 					<div class="text-muted">
 						<p>Av. Las Heras 2555, CABA.<br />
 							(+54- 11) 4808-0090.</p>
+					</div>
+				</a>
+			</div>
+			<div class="col-sm-3 text-center">
+				<a href="https://www.bellasartes.gob.ar/" target="_blank" rel="noopener noreferrer">
+					<img style="max-width: 100%; max-height: 100%;"
+						 src="/themes/colecciones-nacionales/assets/pawtucket/graphics/logo_museo_b_artes_0_0.jpg" alt="">
+					<h3>Museo Nacional de Bellas Artes</h3>
+					<div class="text-muted">
+						<p>Av. Del Libertador 1473, CABA.<br />
+							(+54-11) 5288-9900.</p>
 					</div>
 				</a>
 			</div>
--- themes/colecciones-nacionales/views/pageFormat/pageHeader.php
+++ themes/colecciones-nacionales/views/pageFormat/pageHeader.php
@@ -76,7 +76,7 @@
 ?>
 </head>
 <body>
-		<nav class="navbar" role="navigation">
+		<nav class="navbar navbar-default yamm" role="navigation">
 			<div class="container menuBar">
 				<!-- Brand and toggle get grouped for better mobile display -->
 				<div class="navbar-header">
